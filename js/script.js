ed6d67bc2e51c66a8dcdfe5db2c6b2d0253a781d
mahmud035 <mahamudulhasan4148@gmail.com>
2022-08-22 18:16:39 +0600

    switched dark mode & light mode text normal & solid moon icon

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@
 document.getElementById('btn-toggle').addEventListener('click', () => {
   const bodyElement = document.querySelector('body');
   bodyElement.classList.toggle('dark');
+
+  const btnToggleText = document.getElementById('btn-toggle-text');
+  const moonIcon = document.getElementById('moon-icon');
+
+  if (bodyElement.classList.contains('dark')) {
+    btnToggleText.innerText = 'Light Mode';
+    // add 'bxs-moon' class for solid moon icon
+    moonIcon.classList.add('bxs-moon');
+    moonIcon.classList.remove('bx-moon');
+  } else {
+    btnToggleText.innerText = 'Dark Mode';
+    moonIcon.classList.add('bx-moon');
+    // remove 'bxs-moon' class for regular moon icon when dark mode is off
+    moonIcon.classList.remove('bxs-moon');
+  }
 });
 
 function loadCountries() {
